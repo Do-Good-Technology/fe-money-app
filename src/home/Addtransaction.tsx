0be1d6a2edc2a327fbc40cb8a447d96b576678fa
Zanma <zainurimahmud32@gmail.com>
2021-09-20 15:20:31 +0700

    edit addTransaction

--- src/home/Addtransaction.tsx
+++ src/home/Addtransaction.tsx
@@ -4,17 +4,21 @@ import moment from "moment";
 import { useHistory } from "react-router-dom";
 
 import { NavBar } from "antd-mobile";
-import { Input, DatePicker } from "antd";
+import { DatePicker, Form, Input, Button } from "antd";
 
 import iconBack from "../aset/iconBack.svg";
-import iconChoose from "../aset/iconChoose.svg";
-import iconNote from "../aset/iconNote.svg";
-import iconDate from "../aset/iconDate.svg";
+import addTransactionIconChooseWallet from "../aset/addTransactionIconChooseWallet.svg";
 
 import "./Addtransaction.css";
 
 function Addtransaction() {
   let history = useHistory();
+
+  moment().calendar(null, {
+    sameDay: "[Today]",
+  });
+
+  const [form] = Form.useForm();
 
   return (
     <div>
@@ -29,6 +33,45 @@ function Addtransaction() {
           Add transaction
         </NavBar>
       </div>
+      <div className="chooseWallet-wrap">
+        <p className="cw-label">Choose wallet</p>
+        <img
+          className="cw-img"
+          src={addTransactionIconChooseWallet}
+          alt="icon choose wallet"
+        />
+        <p className="cw-title">Rekening BCA</p>
+      </div>
+
+      <Form
+        className="form-edit"
+        layout="horizontal"
+        form={form}
+        // onFinish={onFinish}
+        initialValues={{
+          name: "Nafian Hanandyawan",
+          email: "nafianhhh@gmail.com",
+        }}
+      >
+        <Form.Item label="Nominal" name="nominal">
+          <Input prefix="Rp " type="number" placeholder="0" />
+        </Form.Item>
+        <Form.Item label="Date" name="date">
+          <DatePicker
+            className="input-content date-addTransaction"
+            defaultValue={moment()}
+            format={"dddd, MMM YY"}
+          />
+        </Form.Item>
+        <Form.Item label="Category" name="category">
+          <Input defaultValue="Choose Category" />
+        </Form.Item>
+        <Form.Item className="edit-button">
+          <Button type="primary" htmlType="submit">
+            Save
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
